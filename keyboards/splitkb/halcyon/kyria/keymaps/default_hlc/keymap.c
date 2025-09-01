213fcd1ecfc1dd19b0e8fc72a4eb51333ab8a7e7
AlexFSmirnov <alex.f.smirnov@gmail.com>
2025-09-02 00:47:09 +0200

    gaming mode

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_GAMING] = LAYOUT_split_3x6_5_hlc(
      KC_F20  , KC_ESC, KC_Q   ,  KC_W  ,   KC_E ,   KC_R ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P   , KC_BSPC,
-     KC_F19  , KC_LSFT, KC_A   ,  KC_S  ,   KC_D ,   KC_F ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,  KC_SCLN, KC_QUOT,
-     KC_LALT  , KC_LCTL, KC_Z  ,  KC_X  ,   KC_C ,   KC_V , KC_B, KC_DEL ,    MS_BTN1, MS_BTN1, KC_N,   KC_M ,  KC_COMM, KC_DOT ,KC_SLSH, KC_ENT,
-                                KC_1, KC_2, KC_3, KC_SPC , SYM    ,    HUB    , KC_ENT , NAV,    FKEYS,  KC_BSLS,
+     KC_TAB  , KC_LSFT, KC_A   ,  KC_S  ,   KC_D ,   KC_F ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,  KC_SCLN, KC_QUOT,
+     KC_LALT  , KC_LCTL, KC_Z  ,  KC_X  ,   KC_C ,   KC_V , KC_B, SYM ,    MS_BTN1, MS_BTN1, KC_N,   KC_M ,  KC_COMM, KC_DOT ,KC_SLSH, KC_ENT,
+                                KC_1, KC_2, KC_SPC , KC_3, KC_4    ,    HUB    , KC_ENT , NAV,    FKEYS,  KC_BSLS,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
